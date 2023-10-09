8c53c7c13baf4247d2e2bedc38c043fa7b85a158
Shubh Gupt <138377735+sept1st2c@users.noreply.github.com>
2023-10-09 17:52:09 +0530

    Update index.js

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -6,20 +6,49 @@ const Navbar = () => {
 
   return (
     <Nav>
-        <NavContainer>
-                <NavLogo>
-                        <img src='./septic2.png' alt='logo'/>
-                </NavLogo>
-                <NavItems>
-                        <NavLink href='#about'>About</NavLink>
-                        <NavLink href='#skills'>Skills</NavLink>
-                        <NavLink href='#Experience'>Achievements</NavLink>
-
-                </NavItems>
-                <ButtonContainer>
-                        <GitHubButton>GitHub Profile</GitHubButton>
-                </ButtonContainer>
-        </NavContainer>
+      <NavbarContainer>
+        <NavLogo to='/'>
+          <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20;', cursor: 'pointer' }}>
+            <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
+          </a>
+        </NavLogo>
+        <MobileIcon>
+          <FaBars onClick={() => {
+            setIsOpen(!isOpen)
+          }} />
+        </MobileIcon>
+        <NavItems>
+          <NavLink href="#about">About</NavLink>
+          <NavLink href='#skills'>Skills</NavLink>
+          <NavLink href='#experience'>Experience</NavLink>
+          <NavLink href='#projects'>Projects</NavLink>
+          <NavLink href='#education'>Education</NavLink>
+        </NavItems>
+        <ButtonContainer>
+          <GitHubButton href={Bio.github} target="_blank">Github Profile</GitHubButton>
+        </ButtonContainer>
+        {
+          isOpen &&
+          <MobileMenu isOpen={isOpen}>
+            <MobileLink href="#about" onClick={() => {
+              setIsOpen(!isOpen)
+            }}>About</MobileLink>
+            <MobileLink href='#skills' onClick={() => {
+              setIsOpen(!isOpen)
+            }}>Skills</MobileLink>
+            <MobileLink href='#experience' onClick={() => {
+              setIsOpen(!isOpen)
+            }}>Experience</MobileLink>
+            <MobileLink href='#projects' onClick={() => {
+              setIsOpen(!isOpen)
+            }}>Projects</MobileLink>
+            <MobileLink href='#education' onClick={() => {
+              setIsOpen(!isOpen)
+            }}>Education</MobileLink>
+            <GitHubButton style={{padding: '10px 16px',background: `${theme.primary}`, color: 'white',width: 'max-content'}} href={Bio.github} target="_blank">Github Profile</GitHubButton>
+          </MobileMenu>
+        }
+      </NavbarContainer>
     </Nav>
   )
 }
@@ -36,6 +65,10 @@ const Nav = styled.div`
         position: sticky;
         top: 0;
         z-index: 10;
+
+        @media (max-width: 960px) {
+        trastion: 0.8s all ease;
+    }
 `;
 const NavContainer = styled.div`
         display: flex;
@@ -54,6 +87,10 @@ const NavLogo = styled(LinkR)`
         cursor: pointer;
         text-decoration: none;
         align-items: center;
+
+        @media (max-width: 640px) {
+      padding: 0 0px;
+  }
 `;
 
 const NavItems =styled.ul `
@@ -63,6 +100,10 @@ const NavItems =styled.ul `
         align-items: center;
         gap: 32px;
         list-style: none;
+
+         @media screen and (max-width: 768px) {
+      display: none;
+    }
 
 `;
 const NavLink=styled.a `
@@ -82,6 +123,10 @@ const ButtonContainer=styled.div`
         width: 80%;
         height: 100%;  
         padding: 0 6px;
+
+        @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 const GitHubButton = styled.button`
         background-color: transparent;
@@ -100,5 +145,116 @@ const GitHubButton = styled.button`
                 background-color: ${({ theme }) => theme.primary};
                 color: ${({ theme }) => theme.card};
         }
+        @media screen and (max-width: 768px) { 
+    font-size: 14px;
+    }
+`;
+
+const MobileIcon = styled.div`
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 60%);
+    font-size: 1.5rem;
+    cursor: pointer;
+    color: ${({ theme }) => theme.text_primary};
+  }
+`
+
+const MobileMenu = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 16px;
+    position: absolute;
+    top: 80px;
+    right: 0;
+    width: 100%;
+    padding: 12px 40px 24px 40px;
+    background: ${({ theme }) => theme.card_light+99};
+    transition: all 0.6s ease-in-out;
+    transform: ${({ isOpen }) => (isOpen ? 'translateY(0)' : 'translateY(-100%)')};
+    border-radius: 0 0 20px 20px;
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+    opacity: ${({ isOpen }) => (isOpen ? '100%' : '0')};
+    z-index: ${({ isOpen }) => (isOpen ? '1000' : '-1000')};
+
+`
+
+const MobileMenuItems = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 32px;
+  list-style: none;
+  width: 100%;
+  height: 100%;
+`
+
+const MobileMenuLink = styled(LinkR)`
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  :hover {
+    color: ${({ theme }) => theme.primary};
+  }
+
+  &.active {
+    border-bottom: 2px solid ${({ theme }) => theme.primary};
+  }
+`;
+
+const MobileMenuButton = styled.a`
+  border: 1.8px solid ${({ theme }) => theme.primary};
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  height: 70%;
+  border-radius: 20px;
+  color: ${({ theme }) => theme.primary};
+  cursor: pointer;
+  padding: 0 20px;
+  font-weight: 500;
+  text-decoration: none;
+  font-size: 16px;
+  transition: all 0.6s ease-in-out;
+
+  :hover {
+    background: ${({ theme }) => theme.primary};
+    color: ${({ theme }) => theme.white};
+  }
+`;
+
+const MobileLink = styled.a`
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  :hover {
+    color: ${({ theme }) => theme.primary};
+  }
+
+  &.active {
+    border-bottom: 2px solid ${({ theme }) => theme.primary};
+  }
+`;
+
+const MobileNavLogo = styled(LinkR)`
+  width: 80%;
+  padding: 0 6px;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  text-decoration: none;
+  @media (max-width: 640px) {
+    padding: 0 0px;
+  }
 `;
 
